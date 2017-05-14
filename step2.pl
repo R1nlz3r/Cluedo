87511f03cb4ffb081de3bb1195a5ea91ccf2b770
Mathis Pandelé <mapandel@student.42.fr>
2017-05-14 14:55:43 +0200

    step2.pl 1.0 : Fonctionnel

--- step2.pl
+++ step2.pl
@@ -1,24 +1,26 @@
-result(Problem) :-
-	Problem =
-	[
-		maison(CouleurA, NatA, CigA, BoissonA, AnimalA, 1),
-		maison(CouleurB, NatB, CigB, BoissonB, AnimalB, 2),
-		maison(CouleurC, NatC, CigC, BoissonC, AnimalC, 3),
-		maison(CouleurD, NatD, CigD, BoissonD, AnimalD, 4),
-		maison(CouleurE, NatE, CigE, BoissonE, AnimalE, 5)
-	],
-member(maison(rouge, anglais, _, _, _, _), Problem),
-member(maison(_, suedois, _, _, chiens, _), Problem),
-member(maison(_, danois, _, the, _, _), Problem),
-// member(maison(verte, _, _, _, _, _), Problem),
-member(maison(verte, _, _, cafe, _, _), Problem),
-member(maison(_, _, pallMall, _, oiseaux, _), Problem),
-member(maison(jaune, _, dunhill, _, _, _), Problem),
-member(maison(_, _, _, lait, _, 3), Problem),
-member(maison(_, norvegien, _, _, _, 1), Problem),
-// member(maison(_, _, blend, _, ??
-// member(maison(_, _, _, _, Cheval ??
-member(maison(_, _, bluemaster, biere, _, _), Problem),
-member(maison(_, allemand, prince, _, _, _), Problem),
-member(maison(_, norvegien ??
-member(??
+a_droite_de(X, Y, [Y,X|_]).
+a_droite_de(X, Y, [_|R]) :- a_droite_de(X, Y, R).
+a_gauche_de(X , Y, [X,Y|_]).
+a_gauche_de(X , Y, [_|R]) :- a_gauche_de(X, Y, R).
+a_cote_de(X, Y, Z) :- a_droite_de(X, Y, Z); a_gauche_de(X, Y, Z).
+
+probleme(Maisons) :- Maisons = [[_, _, _, _, _, 1], [_, _, _, _, _, 2],
+	[_, _, _, _, _, 3], [_, _, _, _, _, 4], [_, _, _, _, _, 5]],
+	member([rouge, anglais, _, _, _, _], Maisons),
+	member([_, suedois, _, _, chiens, _], Maisons),
+	member([_, danois, _, the, _, _], Maisons),
+	a_gauche_de([verte, _, _, _, _, _], [blanche, _, _, _, _, _], Maisons),
+	member([verte, _, _, cafe, _, _], Maisons),
+	member([_, _, pall_mall, _, oiseaux, _], Maisons),
+	member([jaune, _, dunhill, _, _, _], Maisons),
+	member([_, _, _, lait, _, 3], Maisons),
+	member([_, norvegien, _, _, _, 1], Maisons),
+	a_cote_de([_, _, blend, _, _, _], [_, _, _, _, chats, _], Maisons),
+	a_cote_de([_, _, _, _, cheval, _], [_, _, dunhill, _, _, _], Maisons),
+	member([_, _, blue_master, biere, _, _], Maisons),
+	member([_, allemand, prince, _, _, _], Maisons),
+	a_cote_de([bleue, _, _, _, _, _], [_, norvegien, _, _, _, _], Maisons),
+	a_cote_de([_, _, blend, _, _, _], [_, _, _, eau, _, _], Maisons).
+
+qui_a_le_poisson(Nationalite) :- probleme(Maisons),
+	member([_, Nationalite, _, _, poisson, _], Maisons).
