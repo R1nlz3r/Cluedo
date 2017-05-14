cf537ab15639dafad80f9a4f8ab294fc1ce4760f
Natacha OZANNE <nozanne@e2r5p20.42.fr>
2017-05-14 12:33:28 +0200

    starts step2

--- step2.pl
+++ step2.pl
@@ -8,3 +8,17 @@ result(Problem) :-
 		maison(CouleurE, NatE, CigE, BoissonE, AnimalE, 5)
 	],
 member(maison(rouge, anglais, _, _, _, _), Problem),
+member(maison(_, suedois, _, _, chiens, _), Problem),
+member(maison(_, danois, _, the, _, _), Problem),
+// member(maison(verte, _, _, _, _, _), Problem),
+member(maison(verte, _, _, cafe, _, _), Problem),
+member(maison(_, _, pallMall, _, oiseaux, _), Problem),
+member(maison(jaune, _, dunhill, _, _, _), Problem),
+member(maison(_, _, _, lait, _, 3), Problem),
+member(maison(_, norvegien, _, _, _, 1), Problem),
+// member(maison(_, _, blend, _, ??
+// member(maison(_, _, _, _, Cheval ??
+member(maison(_, _, bluemaster, biere, _, _), Problem),
+member(maison(_, allemand, prince, _, _, _), Problem),
+member(maison(_, norvegien ??
+member(??
